f723adf50eabaa4653c23be9fd021e036e1b72ea
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-10-09 17:51:20 +0700

    Bai_4.1.updateMainClass

--- Bai_4.1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Bai_4.1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
 
+public class MainActivity extends AppCompatActivity {
+    EditText edtC, edtF;
+    Button btnCF, btnFC, btnClear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,36 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edtC = findViewById(R.id.edtC);
+        edtF = findViewById(R.id.edtF);
+        btnCF = findViewById(R.id.btnCF);
+        btnFC = findViewById(R.id.btnFC);
+        btnClear = findViewById(R.id.btnClear);
+        btnCF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DecimalFormat dcF = new DecimalFormat("#.00");
+                String doC = edtC.getText()+"";
+                int C = Integer.parseInt(doC);
+                edtF.setText(""+dcF.format(C*1.8+32));
+            }
+        });
+        btnFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DecimalFormat dcF = new DecimalFormat("#.00");
+                String doF = edtF.getText()+"";
+                int F = Integer.parseInt(doF);
+                edtC.setText(""+dcF.format((F-32)/1.8));
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtC.setText("");
+                edtF.setText("");
+            }
         });
     }
 }
