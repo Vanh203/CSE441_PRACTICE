7f19a386701c82827f31ebd3caad2c444b5ff8ba
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-10-09 19:04:27 +0700

    Bai_4.1.updateMainClass

--- Bai_4.2/app/src/main/java/com/example/bmi/MainActivity.java
+++ Bai_4.2/app/src/main/java/com/example/bmi/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.bmi;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
 
+public class MainActivity extends AppCompatActivity {
+    Button btnChuanDoan;
+    EditText edtTen, edtCanNang, edtChieuCao, edtBMI, edtChuanDoan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,39 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnChuanDoan = findViewById(R.id.btn_BMI);
+        edtTen = findViewById(R.id.edt_name);
+        edtCanNang = findViewById(R.id.edt_can_nang);
+        edtChieuCao = findViewById(R.id.edt_chieu_cao);
+        edtBMI = findViewById(R.id.edt_BMI);
+        edtChuanDoan = findViewById(R.id.edt_chuan_doan);
+        btnChuanDoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double H = Double.parseDouble(edtChieuCao.getText()+"");
+                double W = Double.parseDouble(edtCanNang.getText()+"");
+                double BMI = W / (H * H);
+                Log.d("BMI", BMI + "");
+                String chuandoan = "";
+
+                if (BMI < 18) {
+                    chuandoan = "Bạn gầy";
+                } else if (BMI < 24.9) {
+                    chuandoan = "Bạn bình thường";
+                } else if (BMI <= 29.9) {
+                    chuandoan = "Bạn béo phì độ 1";
+                } else if (BMI <= 34.9) {
+                    chuandoan = "Bạn béo phì độ 2";
+                } else {
+                    chuandoan = "Bạn béo phì độ 3";
+                }
+
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                edtBMI.setText(dcf.format(BMI));
+                edtChuanDoan.setText(chuandoan);
+            }
         });
     }
 }
