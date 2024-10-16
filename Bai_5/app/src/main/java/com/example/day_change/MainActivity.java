4f0c8f935fba55a2b9a260d522e5e7c746086eeb
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-10-16 13:58:48 +0700

    commit MainActivity

--- Bai_5/app/src/main/java/com/example/day_change/MainActivity.java
+++ Bai_5/app/src/main/java/com/example/day_change/MainActivity.java
@@ -14,6 +14,39 @@ public class MainActivity extends AppCompatActivity {
     Button button1;
     TextView textview4;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        editnamduonglich = findViewById(R.id.editnamduonglich);
+        button1 = findViewById(R.id.button1);
+        textview4 = findViewById(R.id.textview4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int namduong = Integer.parseInt(editnamduonglich.getText().toString());
+                    String can = getCan(namduong);
+                    String chi = getChi(namduong);
+                    String namamlich = can + " " + chi;
+                    textview4.setText(namamlich);
+                } catch (NumberFormatException e) {
+                    editnamduonglich.setError("Vui lòng nhập năm dương lịch");
+                }
+            }
+        });
+    }
+
+    private String getCan(int namduong) {
+        String[] can = {"Canh", "Tân", "Nhâm", "Quý", "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ"};
+        return can[namduong % 10];
+    }
+
+    private String getChi(int namduong) {
+        String[] chi = {"Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mẹo", "Thìn", "Tỵ", "Ngọ", "Mùi"};
+        return chi[namduong % 12];
+    }
 
 }
