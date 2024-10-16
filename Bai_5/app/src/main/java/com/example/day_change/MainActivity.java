e420e8bc8681438e8d3003dcdc246993f88d5fc6
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-10-16 13:54:34 +0700

    commit MainActivity

--- Bai_5/app/src/main/java/com/example/day_change/MainActivity.java
+++ Bai_5/app/src/main/java/com/example/day_change/MainActivity.java
@@ -1,24 +1,19 @@
 package com.example.day_change;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+    EditText editnamduonglich;
+    Button button1;
+    TextView textview4;
+
+
+
 }
