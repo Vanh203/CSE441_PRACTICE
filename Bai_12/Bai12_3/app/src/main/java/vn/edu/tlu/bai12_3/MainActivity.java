6a65ed419fd242d42774f010a97f2c4f3c1b48e2
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-10-25 02:29:09 +0700

    commit

--- Bai_12/Bai12_3/app/src/main/java/vn/edu/tlu/bai12_3/MainActivity.java
+++ Bai_12/Bai12_3/app/src/main/java/vn/edu/tlu/bai12_3/MainActivity.java
@@ -1,6 +1,11 @@
 package vn.edu.tlu.bai12_3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView txtChoice;
+    String[] array = {"Java", "Kotlin", "Python", "JavaScript", "C++"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Spinner spinner = findViewById(R.id.spinner);
+        txtChoice = findViewById(R.id.textView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, array);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                txtChoice.setText(array[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                txtChoice.setText("");
+            }
         });
     }
 }
