6f319ab794c50968f813c166f8a8e66aa3a895b2
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-10-25 01:05:49 +0700

    commit bai 9

--- Bai_9/app/src/main/java/vn/edu/tlu/intent_services/MyService.java
+++ Bai_9/app/src/main/java/vn/edu/tlu/intent_services/MyService.java
@@ -1,5 +1,41 @@
 package vn.edu.tlu.intent_services;
 
-public class MyService {
-    
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import vn.edu.tlu.intent_services.R;
+
+public class MyService extends Service {
+    MediaPlayer mediaPlayer;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer.setLooping(true);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if(mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        } else {
+            mediaPlayer.start();
+        }
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+    }
 }
