db5e6444ddc76f5ab08a9f30e8ec2bee0a86442f
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-10-25 00:37:57 +0700

    commit Bai8_1

--- Bai_8/Bai_8.1/app/src/main/java/vn/edu/tlu/intent_call_sms/CallActivity.java
+++ Bai_8/Bai_8.1/app/src/main/java/vn/edu/tlu/intent_call_sms/CallActivity.java
@@ -1,4 +1,46 @@
 package vn.edu.tlu.intent_call_sms;
 
-public class CallActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CallActivity extends AppCompatActivity {
+    Button btnBack;
+    ImageButton imgBtnCall;
+    EditText editPhoneNum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_call);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        imgBtnCall = findViewById(R.id.image_btn_send_sms);
+        btnBack = findViewById(R.id.btn_back);
+        editPhoneNum = findViewById(R.id.edit_sms);
+
+        imgBtnCall.setOnClickListener(
+                v-> {
+                    String phoneNumber  = editPhoneNum.getText().toString();
+                    Intent intent = new Intent(Intent.ACTION_CALL , Uri.parse("tel:" + phoneNumber));
+                    if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != 0){
+                        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CALL_PHONE}, 1);
+                    }
+                    startActivity(intent);
+                } );
+        btnBack.setOnClickListener(v -> finish());
+    }
 }
