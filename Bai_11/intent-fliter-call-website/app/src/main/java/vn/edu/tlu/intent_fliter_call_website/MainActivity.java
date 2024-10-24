c5c1eb56cee7f873b1d36c5b91c3852f6c9b8098
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-10-25 01:48:32 +0700

    commit

--- Bai_11/intent-fliter-call-website/app/src/main/java/vn/edu/tlu/intent_fliter_call_website/MainActivity.java
+++ Bai_11/intent-fliter-call-website/app/src/main/java/vn/edu/tlu/intent_fliter_call_website/MainActivity.java
@@ -1,6 +1,11 @@
 package vn.edu.tlu.intent_fliter_call_website;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSearch;
+    EditText txtSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnSearch = findViewById(R.id.btn_search);
+        txtSearch = findViewById(R.id.edit_text_search);
+        btnSearch.setOnClickListener(v -> {
+            String search = txtSearch.getText().toString();
+            if (!search.isEmpty()) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("https:%s", search)));
+
+                startActivity(intent);
+            }else {
+                Toast.makeText(this, "Please enter a search query", Toast.LENGTH_SHORT).show();
+                return;
+            }
         });
     }
 }
