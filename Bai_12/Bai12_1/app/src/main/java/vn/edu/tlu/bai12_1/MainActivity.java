0837d757f31375d189dc0124a2d5cfc15eaa1c3b
Đặng Việt Anh <2151170558@e.tlu.edu.vn>
2024-10-25 02:05:18 +0700

    commit

--- Bai_12/Bai12_1/app/src/main/java/vn/edu/tlu/bai12_1/MainActivity.java
+++ Bai_12/Bai12_1/app/src/main/java/vn/edu/tlu/bai12_1/MainActivity.java
@@ -1,6 +1,10 @@
 package vn.edu.tlu.bai12_1;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lvPhone;
+    TextView txtTitle;
+    String[] phones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        phones = getResources().getStringArray(R.array.phones);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        lvPhone = findViewById(R.id.lv_phones);
+        txtTitle = findViewById(R.id.txt_title);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, phones);
+        lvPhone.setAdapter(adapter);
+        lvPhone.setOnItemClickListener((parent, view, position, id) -> txtTitle.setText("Position: " + position + " - " + phones[position]));
     }
 }
